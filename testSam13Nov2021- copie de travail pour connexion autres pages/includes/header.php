983f109b97d7a4cddf9b192e79ff548f14850679
thomas-ayissi <thomas8sea@gmail.com>
2021-11-15 01:13:39 +0100

    Initial commit

---
+++ testSam13Nov2021- copie de travail pour connexion autres pages/includes/header.php
@@ -0,0 +1,26 @@
+<?php
+include_once('variables.php');
+include_once('functions.php');
+?>
+
+
+
+
+<div id="logo"><img src="../decor/logo_thomas.png"></div>
+
+<nav id="header">
+    <div id="menu">
+        <h3></h3>
+        <ul>
+            <li><a href="../index.php">Accueil</a></li>
+            <li><a href="../pages_affichage/galerie.php">Galerie</a></li>
+            <li><a href="../pages_affichage/liens.php">Liens</a></li>
+            <li><a href="../pages_affichage/tutoriels.php">Tutoriels</a></li>
+            <li><a href="../pages_affichage/contact.php">Contact</a></li>
+            <li><a href="../pages_affichage/admin.php">Admin</a></li>
+            <li><a href="../pages_affichage/cv.php">CV</a></li>
+
+        </ul>
+    </div>
+</nav>
+
